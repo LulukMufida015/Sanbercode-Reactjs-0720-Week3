893965f4bf5a896aae6af82dc6a2728b246834f0
Luluk Mufida <lulukmufida0015@gmail.com>
2020-08-03 18:20:46 +0700

    Belajar Component dan Props

---
+++ pekan3/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Tugas11 from './Tugas11/InfoTabel'
+import InfoTabel from './Tugas11/InfoTabel';
+
+function App() {
+  return (
+    <div>
+      <InfoTabel />
+    </div>
+  );
+}
+
+export default App;
